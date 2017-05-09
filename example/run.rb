81ea6f42a5550ec5d7b0937cd6f2620cf436fbd6
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-05-09 16:09:32 +0100

    Add another order to show output in the command line

--- example/run.rb
+++ example/run.rb
@@ -16,13 +16,29 @@ broadcaster_5 = Broadcaster.new(5, 'Channel 4')
 broadcaster_6 = Broadcaster.new(6, 'Bike Channel')
 broadcaster_7 = Broadcaster.new(7, 'Horse and Country')
 
-material = Material.new('WNP/SWCL001/010')
+material_1 = Material.new('WNP/SWCL001/010')
+material_2 = Material.new('ZDW/EOWW005/010')
 
-order = Order.new(material)
+order_1 = Order.new(material_1)
+order_2 = Order.new(material_2)
 
-order.add broadcaster_1, standard_delivery
-order.add broadcaster_2, standard_delivery
-order.add broadcaster_3, express_delivery
 
-print order.output
+#  send `WNP/SWCL001/010` to Disney, Discovery, Viacom via Standard Delivery and Horse and Country via Express Delivery
+# based on the defined Discounts the total should be $45.00
+order_1.add broadcaster_1, standard_delivery
+order_1.add broadcaster_2, standard_delivery
+order_1.add broadcaster_3, standard_delivery
+order_1.add broadcaster_7, express_delivery
+
+# * send `ZDW/EOWW005/010` to Disney, Discovery, Viacom via Express Delivery
+  #  based on the defined Discounts the total should be $40.50
+
+order_2.add broadcaster_1, express_delivery
+order_2.add broadcaster_2, express_delivery
+order_2.add broadcaster_3, express_delivery
+
+
+print order_1.output
+print "\n"
+print order_2.output
 print "\n"

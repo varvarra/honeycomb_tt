0f9b82edd8d7b46e5bf8a465b2c7e41ec7c73757
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-05-10 17:37:32 +0100

    Add test for 'add' method

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,11 +3,20 @@ require './models/delivery'
 require './models/material'
 require './models/order'
 
+
 describe Order do
   subject { Order.new material }
   let(:material) { Material.new 'HON/TEST001/010' }
   let(:standard_delivery) { Delivery.new(:standard, 10) }
   let(:express_delivery) { Delivery.new(:express, 20) }
+  let(:broadcaster_1) {Broadcaster.new(1, 'Disney') }
+
+  describe 'class' do
+    it 'creates an instance of a class' do
+      expect(subject).to be_a(Order)
+    end
+  end
+
 
   context 'empty' do
     it 'costs nothing' do
@@ -26,4 +35,16 @@ describe Order do
       expect(subject.total_cost).to eq(30)
     end
   end
+
+  describe "#add" do
+    it { is_expected.to respond_to(:add).with(2).arguments }
+
+    it "changes the number of elements in the array of items" do
+      subject.add(broadcaster_1, standard_delivery)
+      expect(subject.items).to include [broadcaster_1, standard_delivery]
+    end
+
+  end
+
+
 end

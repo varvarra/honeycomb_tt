14bf5e85aa45e5d6d2c042f9bb63b1ae6fc4fb1f
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-05-11 17:01:00 +0100

    On initializaion functionality. Tested

--- models/discount.rb
+++ models/discount.rb
@@ -2,4 +2,15 @@
 
 class Discount
 
+  attr_reader :discount_amount, :min_spend, :deliv_type, :price_decrease, :delvery_min
+
+  def initialize(settings = {})
+    @discount_percent = settings[:discount_amount] || 10
+    @min_spend = settings[:min_spend] || 30
+    @deliv_type = settings[:deliv_type] || :express
+    @price_change = settings[:price_change] || 5
+    @delvery_min = settings[:delvery_min] || 2
+  end
+
+
 end
--- spec/discount_spec.rb
+++ spec/discount_spec.rb
@@ -5,11 +5,25 @@ describe Discount do
 
   subject { Discount.new }
 
-  describe 'class' do
     it 'creates an instance of a class' do
       expect(subject).to be_a(Discount)
     end
-  end
+
+    describe "#initalize" do
+      it 'can be initialised with paraters passed as argments' do
+        settings = {:discount_percent => 5,
+                    :min_spend => 35,
+                    :deliv_type => :standard,
+                    :price_change => 4,
+                    :delvery_min => 3}
+        expect{(Discount.new(settings))}.to_not raise_error
+      end
+
+      it 'can be initialised with default parameters' do
+        expect{(Discount.new)}.to_not raise_error
+      end
+    end
+
 
 
 end

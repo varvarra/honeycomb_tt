498fb5b9729497f8e4a8e431ce55c21b47e9d679
Barbara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-05-17 16:19:50 +0100

    Added working and teste discount functionality

--- models/discount.rb
+++ models/discount.rb
@@ -1,16 +1,36 @@
 # should know everything about discount, when they apply, and how to apply them on order
 
+# require relative './models/order.rb'
+
 class Discount
 
-  attr_reader :discount_amount, :min_spend, :deliv_type, :price_decrease, :delvery_min
+  attr_reader :discount_percent, :min_spend, :deliv_type, :price_decrease, :delivery_min, :percentage, :error_msg
 
   def initialize(settings = {})
-    @discount_percent = settings[:discount_amount] || 10
+    @discount_percent = settings[:discount_percent] || 10
     @min_spend = settings[:min_spend] || 30
     @deliv_type = settings[:deliv_type] || :express
-    @price_change = settings[:price_change] || 5
-    @delvery_min = settings[:delvery_min] || 2
+    @price_decrease= settings[:price_decrease] || 5
+    @delivery_min = settings[:delivery_min] || 2
+    @error_msg = settings[:error_msg] || "This order already has a discount"
+    @percentage = (100.to_f - @discount_percent) / 100
   end
 
+  def update_total(total, deliveries)
+    if deliveries.count(deliv_type) >= delivery_min
+      apply_percent(change_price(total, deliveries))
+    else
+      apply_percent(total)
+    end
+  end
+
+
+  def change_price(total, deliveries)
+    total - (deliveries.count(deliv_type)*price_decrease)
+  end
+
+  def apply_percent(total)
+    total > min_spend ? total * percentage : total
+  end
 
 end
--- spec/discount_spec.rb
+++ spec/discount_spec.rb
@@ -1,10 +1,10 @@
 require './models/discount'
 
-
 describe Discount do
 
   subject { Discount.new }
-
+  deliveries = [:express, :express, :express]
+  total = 60
     it 'creates an instance of a class' do
       expect(subject).to be_a(Discount)
     end
@@ -15,7 +15,9 @@ describe Discount do
                     :min_spend => 35,
                     :deliv_type => :standard,
                     :price_change => 4,
-                    :delvery_min => 3}
+                    :delvery_min => 3,
+                    :error_msg => "This order already has a discount"
+                    }
         expect{(Discount.new(settings))}.to_not raise_error
       end
 
@@ -23,6 +25,22 @@ describe Discount do
         expect{(Discount.new)}.to_not raise_error
       end
     end
+
+      it "can check if the order is valid for apply_percent discount" do
+        expect(subject.apply_percent(total)).to eq(54)
+      end
+
+      it "can change the price of the deliviries if they are more than delivery_min" do
+        expect(subject.change_price(total, deliveries)).to eq(45)
+      end
+
+      it "discounts total price with all applicable promotions" do
+        expect(subject.update_total(total, deliveries)).to eq(40.5)
+      end
+
+      it "returns an error message 'This order already has a discount' by default" do
+       expect(subject.error_msg).to eq('This order already has a discount')
+      end
 
 
 
